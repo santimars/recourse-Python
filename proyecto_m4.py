ee1f6bd25e121e4396abbc3e83acda6be86478fc
Santi Rodriguez <santiagoestebanrodriguezgutier@gmail.com>
2023-09-12 14:45:14 -0600

    Update proyecto_m4.py
    Update proyecto_m4.py
    
    Co-Authored-By: Michael ezra <142544467+michael34df@users.noreply.github.com>

--- proyecto_m4.py
+++ proyecto_m4.py
@@ -115,4 +115,59 @@ print(type(mibici).__name__)
 #comprobamos si bicicletaelectrica hereda a vehiculos electricos,vi¿ehiculos y quad
 print(issubclass(Bicicleta_Electrica,Vehiculos_Electricos))
 print(issubclass(Bicicleta_Electrica,vehiculos))
-print(issubclass(Bicicleta_Electrica,Quad))"
+print(issubclass(Bicicleta_Electrica,Quad))
+
+mibici  = Bicicleta_Electrica()
+
+
+''              ''' Entendido, en resumen tenemos dos casos cuando hacemos que una clase de multiples clases'''
+
+'''
+Si la clase que tiene herencia multiple tiene constructor -> lo ejecuta
+si la clase que tiene herencia multiple No tiene contructor -> Ejecuta el constructor del padre principal(el que se encuentra
+mas a la izquierda en la visa de herencia)
+'''
+
+''             '''Y en el caso de qu ela clase que hereda, tenfa constructor(y lo ejecute),puede aun asi
+ejecutar los contructores de sus padres?'''
+
+'''
+SI!! Invocaremos desde el constructor de nuestra clasee a todos los contructores de los padres que queramos 
+'''
+class vehiculos:
+    #contructor
+    def __init__(self,marca,modelo):
+        self.marca = marca
+        self.modelo = modelo
+        print("creando objeto de la clase Vehiculos")
+
+    def repostar(self):
+        print("repostanto combistible")
+
+class Vehiculos_Electricos:
+    #contructor
+    def __init__(self):
+        self.autonomia = 100
+        print("creando objeto de la clase Vehiculos Electricos")
+
+    def cargarEnergia(self):
+        '''Metodo cargarEnergia de la clase Vehiculos Electricos. Indica al sistema si el Vehiculo Electrico esta cargado o no.'''
+        self.cargando = True
+        print("Cargando energia")
+
+class Bicicleta_Electrica(Vehiculos_Electricos,vehiculos):
+    '''Metodo Bicicleta Electrica que se hereda de la clase vehiculos y la clase vehiculos electricos, es decir, herencia multiple.'''
+    def __init__(self,marca=None,modelo=None,autonomia=None):
+        Vehiculos_Electricos.__ini__(self,autonomia) # Equivale a: super().__init__(autonomia)
+        vehiculos.__init__(self,marca,modelo)
+        print("Hemos creado un objeto de la clase Bicicleta Electrica")
+        
+    def __str__(self):
+        return vehiculos.__str__(self) + "\n" + Vehiculos_Electricos.__sr__(self)
+        # Equivale a:
+        # return Vehiculos.__str__(self) + super().__str__()
+        
+mibici = Bicicleta_Electrica(marca="ford",modelo="Xtreme",autonomia=80)
+
+# Si estamo usando herencia multiple procuremos en no usar super() ya que tenemos muchos padres y super()solo nos deja uno
+# es mejor acceder de otra manera    
