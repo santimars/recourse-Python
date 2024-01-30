ff0a92d188da993667e6c821b78547f0b54f7778
Santi Rodriguez <santiagoestebanrodriguezgutier@gmail.com>
2024-01-30 12:20:21 -0500

    Update m.5_parte3.py
    se actualizo la ubicacion del documento

--- m.5_parte3.py
+++ m.5_parte3.py
@@ -10,12 +10,50 @@ import csv
 
 #leer el archivo csv con readre
 
-with open("/Users/santirodriguez/Desktop/02_CSV_data.csv","r") as my_csv_file:
+with open("/Users/santirodriguez/Documents/Python/M5/02_CSV_data.csv","r") as my_csv_file:
     reader = csv.reader(my_csv_file)
     for row in reader:
         print(row)
 
 # podremos leer  y escribir en csv
 # es normal leer los ficheros csv linea a linea 
- 
+print("\t")
+# leer archivo csv con reader y optimizando la salida
+with open("/Users/santirodriguez/Documents/Python/M5/02_CSV_data.csv","r") as my_csv_file:
+    reader = csv.reader(my_csv_file, delimiter=";")
+    for row in reader:
+        print(",".join(row))
 
+print("\t")
+#leer solo cabezeras
+with open ("/Users/santirodriguez/Documents/Python/M5/02_CSV_data.csv","r") as f:
+    reader = csv.reader(f)
+    cabeceras = next(reader)
+print(cabeceras)
+
+
+# Uso de reader y next
+
+with open("/Users/santirodriguez/Documents/Python/M5/02_CSV_data.csv","r") as my_csv_file:
+    reader = csv.reader(my_csv_file, delimiter=",")
+    reg0 = next(reader) # leer registro (lista)
+    print(reg0)
+    reg1 = next(reader) # leer siguiente (lista)
+    print(reg1)
+    nombre, provincia, consumo = next(reader) # Almacenar en variables el siguiente
+                                              # (en el oreden que se indica)
+    print("Nombre: ",nombre,"Provincia: ",provincia,"consumo: ",consumo) # Mostrar las variables
+
+
+# leer archico csv con reader, next, bucle y formato de salida
+    
+with  open("/Users/santirodriguez/Documents/Python/M5/02_CSV_data.csv","r") as my_csv_file:
+    reader = csv.reader(my_csv_file, delimiter= ",")
+    next(reader) # saltarnos la cabecera del documento csv
+    for index, row in enumerate(reader):
+        print('lineas: ' + str(index))
+        print('\tnombre: ' + row[0], "provincias: " + row[1], "Consumo:" + row[2])
+
+# Ordenacion por campos con operator
+
+"Documentos en https https://docs.python.org/3/library/operator.html"
