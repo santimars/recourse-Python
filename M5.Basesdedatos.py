f1aa2bf44374d06f62ba0666f3c67004a6978de1
Santi Rodriguez <santiagoestebanrodriguezgutier@gmail.com>
2024-03-16 17:41:20 -0500

    Summary

---
+++ M5.Basesdedatos.py
@@ -0,0 +1,51 @@
+import sqlite3
+'''
+tener en cuenta que todas la aplicaciones tiene una grande cantidad de bases de datos
+por ello es importante en saber manejar mas datos
+
+es mejor diseñar una gran cantidad de datos para que el futuro se preparan para conectar su 
+informacion
+
+ a mayor cantidad de datos esta preparadas para analizar muchos datos para que no sean lentas
+
+
+los programas edducativos se sengundo plano se dejan las bases de datos cuando el 100 por 100 va por 
+las bases de datos 
+
+bases de datos distribuidas
+'''
+# Bases de datos SQLITE
+
+'''
+
+id pedido
+fecha
+id producto
+numero de producto
+precio
+unidades
+si queremo ver los datos en total lo hacemos en programacion 
+la redundancia de datos se trata de que no pase 
+
+'''
+'''
+normalizacion 
+ver los elementos que puedo dividir
+peude ser | pedido y fecha  |   id  nombre producto precio | 
+
+
+podemos ahora organizar en que areas hacen parte para pedido y cuales son mas revelantes para producto
+
+ahora como unimos estas 2 tablas
+vemos que estan exactamente iguales
+hay quehacer una relacion para que las tablas se relaciones
+
+esta ls primary key y la formary key 
+una es al principal y la otra es la relacion 
+
+hoy en dia el problema mundial de los datos es la redundancia de datos
+ellos tienen muchas informacion repetida
+
+'''
+
+
