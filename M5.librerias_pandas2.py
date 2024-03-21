e41ec97d4f6045bbeba7b2de28a0a3b1e5400c39
Santi Rodriguez <santiagoestebanrodriguezgutier@gmail.com>
2024-03-21 17:25:44 -0500

    Summary

--- M5.librerias_pandas2.py
+++ M5.librerias_pandas2.py
@@ -1,10 +1,17 @@
+from ast import And
 from cgi import print_arguments
+from traceback import print_tb
+from httpx import head
+from matplotlib.rcsetup import _listify_validator
 import pandas as pd
 import csv as csv
 import matplotlib as mlt
 import numpy as np
 from reflex import cond
 from sqlalchemy import column
+import csv
+import json
+import sqlite3
 
 '''
 Para carga de datos y exploracion trabajaremos con el fichero 
@@ -81,6 +88,17 @@ print(temps_ordenadas[:10])
 df_ordenado = d.sort_values(by=["Temperature (C)","Apparent Temperature (C)"],ascending=False)
 print(df_ordenado)
 print(df_ordenado[:10])
+
+# Otras funciones de formato (para texto)
+d["Summary"].str.upper() #nos transforma en Mayusculas
+d["Summary"].str.lower()# Nos transforma en minusculas
+d["Summary"].str.capitalize()# nos transforma la primera letra de la palabra en Mayuscula
+d["Summary"].str.title()# Nos transforma en titulo como esta originalmente osea Primera Letra de una palabra en Mayusulas
+d["Summary"].str.swapcase()# Nos trnasforma la primera letra en minusculas y el resto en mayusculas
+d["Summary"].str.contains("Cloudy") # Nos muestra en un True o False ya que evalua todas las palabras para si ver si cumple nuestra condicion
+d["Summary"].unique() # nos muestra los datos unicos de esa columna
+print( len( d["Summary"].unique() ))
+
 
 
 # Para obtener un histograma, la distribucion de los datos de una columna:
@@ -162,7 +180,7 @@ print(d.loc[10:15,["Humidity","Temperature (C)"]])
 
 print(d.loc[10]) # esto es equivalente a d.loc[10, : ]
 print(d.loc[10:12]) #esto es equivalente a d.loc[10, 12, :]
-# sirve para hacer condiciones y tambien como una forma de sclising
+# sirve para hacer condiciones y tambien como una forma de slicing
 
 '''
 iloc : Herrmaienta muy similar a loc
@@ -175,4 +193,99 @@ print(d.iloc[:,0])# Primera columna
 
 print(d.iloc[0:5]) # Primera cinco filas 
 # Esto seria igual que con el loc
- 
+print(d.iloc[2300:2330,0:3])
+print (d.iloc[[0,2,1]]) # primera, tercera y segunda fila
+print(d.iloc[:,[0,2,1]]) # Primera, tercera y senga columnas
+print(d.iloc[:-1])
+# iloc no podemos poner condiciones como == =! >=, si queremos ponerlo debemos usar loc
+
+'''
+Agrupaciones (Groupby)
+En muchos casos  es muy interesante realizar agrupaciones en base a un campo (sexo, pais, origen,etc)
+'''
+grouped_df = d.groupby("Summary")
+print(type(grouped_df)) # Es un objeto
+print(grouped_df)
+# Mostremos el primer registro de cada uno
+print(grouped_df.first())
+print("Numero de grupos: ",len(grouped_df))
+
+# veamos las caracteristicas y sus conincidencias
+print(grouped_df.size()) 
+
+# Preguntemos por una categoria
+grouped_df.get_group("Light Rain")["Humidity"].count()
+grouped_df.get_group("Light Rain")["Humidity"].mean()
+print(d.groupby("Summary"))
+
+group = d.groupby(["Summary","Precip Type"]) # puedo hacer agrupaciones con diferentes grupos
+print(group)
+
+'''
+Conversion
+Es ocasiones, se nesecita una serie de pandas en una lista poara poder trabajar de forma nativa
+desde Python
+'''
+
+temps = d["Temperature (C)"]
+print(type(temps)) # Como se ve es una serie de panda
+temps_list = d["Temperature (C)"].tolist()
+print(type(temps_list))
+
+print("\nListado de temperaturas")
+for t in temps_list:
+    print(round(t,3))
+
+"""
+Exportacion 
+
+vamos a seleccionar un trozo de nuestro CSV y vamos a exportarlo en varios formatos
+"""
+df_ordenado = d.sort_values(by=["Temperature (C)"],ascending=False)
+df_final = df_ordenado.iloc[0:100,0:5] #    primeras 100 filas y primeras 5 columnas
+print(df_final)
+
+media_humedad = d["Humidity"].mean()
+
+condicion = (df_ordenado["Humidity"] <= media_humedad) & (df_ordenado["Summary"].str.contains("cloudy",case=False))
+df_final  = df_ordenado.loc[condicion,:]
+print(df_final)
+
+df_final = df_final.reset_index(drop=True)
+
+df_final = df_final.iloc[0:100,[1,5,9,2]]
+
+
+# Creamos una nueva columna calculada
+d["TempLevel"] = 0  # valor inicial para todas las filas
+
+condicion_positiva = d["Temperature (C)"] > 0
+condicion_negativa = d["Temperature (C)"]<= 0
+listado_columna = ["TempLevel"]
+d.loc[condicion_positiva,listado_columna] = 1
+d.loc[condicion_negativa,lista_columnas] = 0
+
+#d.drop([4],axis=0)
+#d.drop([4,10],axis=0)
+#d.drop([range(4,8,1)],axis = 0)
+#d.drop(["Summary", "TempLevel"],axis = 1)
+# axis = 0  filas & axis =  1 columnas
+'''
+Exportacion a CSV
+'''
+ruta_csv = r"./EXPORT_weather.csv" #r = raw que en spanish es crudo
+df_final.to_csv(ruta_csv, index= False, header = True, sep= ",")
+
+"Exportacion a Excell"
+ruta_excell = r"./EXPORT_weather.xlsx" 
+df_final.to_excel(ruta_excell, index= False, header= True,sheet_name= 'Tiempo')
+
+'''
+Exportacion a JSON
+'''
+ruta_json =r"./EXPORT_weather.json"
+df_final.to_json(ruta_json,orient="columns")
+
+"Exportacion a HTML"
+ruta_html = r"./EXPORT_weahter.html"
+df_final.to_html(ruta_html,index=False)
