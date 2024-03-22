d3814e6be704d9c3834ee8603ffd7cda35762dae
Santi Rodriguez <santiagoestebanrodriguezgutier@gmail.com>
2024-03-21 21:57:40 -0500

    Summary
    Damos inicio a nuestro pequeño projecto

--- M6/main.py
+++ M6/main.py
@@ -8,12 +8,16 @@ vamos a ver que el trabajo que estamos viendo esta
 muy alineada con la programacion orientada a objetos
 todos de esto vamos a reutilizarlo al 100%
 vamos a empezar con la base de datos.
+aqui vamos a guardar la siguiente informacion
 
-main.py ->
-venv -> 
-models.py ->
-db.py ->
-database ->
+main.py ->  nuestro fichero principal
+venv ->  nuestra carpeta de entorno virtual
+models.py -> va a tener nuestras clases
+db.py -> va a tener la configuracion de la base de datos
+database -> va a tener la base de datos
+
+hay mas carpetas pero pronto lo veremos
+este es un projecto pequeño y es lo que deberian tener todos los proyectos
 
 son nombres de convenio donde se llamaran nuestros siguientes projectos futuros
 
